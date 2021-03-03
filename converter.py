38c3e6522c0ac8fab4cd95346e61b4783e3e7d6a
Prometheus <prometheus3375@gmail.com>
2021-03-03 18:09:06 +0300

    Converter uses CPU

--- converter.py
+++ converter.py
@@ -11,8 +11,7 @@ Default_Shape = (1, 3, 256, 512)
 def pytorch2onnx(model: nn.Module, output_file: str, input_shape: List[int] = Default_Shape):
     # Export to ONNX
     inputs = torch.ones(*input_shape)
-    inputs = inputs.cuda()
-    torch.onnx.export(model.eval(), inputs, output_file, opset_version=11)
+    torch.onnx.export(model.eval().cpu(), inputs, output_file, opset_version=11)
 
     # Simplify ONNX
     model = onnx.load(output_file)
@@ -46,8 +45,6 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     model = torch.load(args.model_file)
 
